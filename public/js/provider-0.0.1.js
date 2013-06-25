b9ac7e690b172ad2b089373445d0769932e89e8e
Zack Gilbert <zackgilbert@gmail.com>
2013-06-24 21:01:20 -0500

    Remove marker/overlay and replay it if we still want to track.

--- public/js/provider-0.0.1.js
+++ public/js/provider-0.0.1.js
@@ -361,37 +361,29 @@
     if (is_viewing_map) {
       log("Is Viewing Map: " + current_appointment_id + " == " + $.track.appointment_id);
       if ((""+current_appointment_id) == (""+$.track.appointment_id)) {
-        var latlng = new google.maps.LatLng(coords.latitude, coords.longitude);
-        if (p = $("#directions-map").gmap('get', 'overlays').provider) {
-          p.setPosition(latlng);
-          //pr = $("#directions-map").gmap('get', 'overlay').radius;
-          //pr.setCenter(latlng);
-          //pr.setRadius(coords.accuracy);
-        } else {
-          //$("#directions-map").gmap('addMarker', { 'id': 'provider', 'position': latlng, 'bounds': true, 'icon' : 'http://i.stack.imgur.com/orZ4x.png' });
-          $('#directions-map').gmap('addMarker', new google.maps.Marker({
-            'id' : 'provider',
-            position : latlng,
-            bounds : false,
-            icon : {
-              url : 'http://i.stack.imgur.com/orZ4x.png',
-              size : new google.maps.Size(22, 22),
-              origin : new google.maps.Point(0, 0),
-              anchor : new google.maps.Point(11,11)
-            }
-          }));
-          $("#directions-map").gmap('addShape', 'Circle', {
-            'id' : 'radius',
-            center : latlng,
-            radius : coords.accuracy,
-            fillColor : '#0000cc',
-            fillOpacity : 0.25,
-            strokeColor : '#0000cc',
-            strokeOpacity : 0.5
-          });
-        }
-      } else {
+        $('#directions-map').gmap('clear', 'markers');
         $('#directions-map').gmap('clear', 'overlays');
+        var latlng = new google.maps.LatLng(coords.latitude, coords.longitude);
+        $('#directions-map').gmap('addMarker', new google.maps.Marker({
+          'id' : 'provider',
+          position : latlng,
+          bounds : false,
+          icon : {
+            url : 'http://i.stack.imgur.com/orZ4x.png',
+            size : new google.maps.Size(22, 22),
+            origin : new google.maps.Point(0, 0),
+            anchor : new google.maps.Point(11,11)
+          }
+        }));
+        $("#directions-map").gmap('addShape', 'Circle', {
+          'id' : 'radius',
+          center : latlng,
+          radius : coords.accuracy,
+          fillColor : '#0000cc',
+          fillOpacity : 0.25,
+          strokeColor : '#0000cc',
+          strokeOpacity : 0.5
+        });
       }
     }
   } // updateProviderOnMap
