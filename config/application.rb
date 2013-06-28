0153432227986cdc61f730cc5a3f27752a3bfad7
Zack Gilbert <zackgilbert@gmail.com>
2013-06-28 18:41:58 -0400

    Precompile assets.

--- config/application.rb
+++ config/application.rb
@@ -57,6 +57,9 @@ module OnTheWay
     # Enable the asset pipeline
     config.assets.enabled = true
 
+    # dont precompile
+    config.assets.initialize_on_precompile = false
+
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
