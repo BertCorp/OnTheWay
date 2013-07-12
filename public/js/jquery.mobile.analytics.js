bb58c195256a6cc91b7a715f26c9122b19898011
Zack Gilbert <zackgilbert@gmail.com>
2013-07-12 11:48:35 -0500

    Fix analytics tracking to include actual url path.

--- public/js/jquery.mobile.analytics.js
+++ public/js/jquery.mobile.analytics.js
@@ -12,7 +12,7 @@ $(document).on('pageshow', '[data-role=page]', function(event, ui) {
   try {
     _gaq.push(['_setAccount', 'UA-42053724-1']);
     if ($.mobile.activePage.attr("data-url")) {
-      _gaq.push(['_trackPageview', $.mobile.activePage.attr("data-url")]);
+      _gaq.push(['_trackPageview', window.location.pathname + "#" + $.mobile.activePage.attr("data-url")]);
     } else {
       _gaq.push(['_trackPageview']);
     }
